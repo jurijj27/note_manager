4734c3d478d0971b7ac9800aebca3ea397a1cca2
jurijj27 <jurijj27@mail.ru>
2025-01-27 23:50:19 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,15 @@
+# Grade 1. Этап 1: Задание 2
+
+username = "jurijj27"
+title = "Заметка 1"
+content = "Освоение программы обучения первого месяца"
+status = "Активная"
+created_date = "27-01-2025"
+issue_date = "22-02-2025"
+
+print(f"Имя пользователя: {username}")
+print(f"Заголовок заметки: {title}")
+print(f"Описание заметки: {content}")
+print(f"Статус заметки: {status}")
+print(f"Дата создания заметки: {created_date[0:5]}")
+print(f"Дедлайн заметки: {issue_date[0:5]}")
